f261928b2d1d4c7b75de06ed18c4a5ff44880575
Catherine Alyssa <catherinealyssar@gmail.com>
2025-11-30 15:21:13 +0700

    feat: Update chart and table width handling in Streamlit app

--- modelling/utils/__init__.py
+++ modelling/utils/__init__.py
@@ -1,11 +1,9 @@
 from .transforms import get_transforms
 from .losses import FocalLoss
-from .visualization import plot_training_history, plot_confusion_matrix
-# from .metrics import *
+# Visualization imports mlflow/matplotlib - only import when needed for training
+# from .visualization import plot_training_history, plot_confusion_matrix
 
 __all__ = [
     'get_transforms', 
     'FocalLoss', 
-    'plot_training_history', 
-    'plot_confusion_matrix'
 ]
