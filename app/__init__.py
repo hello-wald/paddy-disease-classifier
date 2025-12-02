492d17bab94f9bfff36fe4ae87ee5b18a61c2d16
Catherine Alyssa <catherinealyssar@gmail.com>
2025-12-02 08:14:57 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,25 +2,8 @@ from src.model import Classifier
 from src.data_loader import RiceDiseaseDataset
 from config import config
 
-def _lazy_import_train():
-    from src.train import train_model
-    return train_model
-
-def _lazy_import_evaluate():
-    from src.evaluate import evaluate_model, prepare_model_signature
-    return evaluate_model, prepare_model_signature
-
 __all__ = [
     "Classifier",
     "RiceDiseaseDataset",
     "config",
 ]
-
-def train_model(*args, **kwargs):
-    return _lazy_import_train()(*args, **kwargs)
-
-def evaluate_model(*args, **kwargs):
-    return _lazy_import_evaluate()[0](*args, **kwargs)
-
-def prepare_model_signature(*args, **kwargs):
-    return _lazy_import_evaluate()[1](*args, **kwargs)
